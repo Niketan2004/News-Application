c91f69f70441420e88f409e6fbeaa91c409dfda8
Niketan2004 <Koyandeniketans2004@gmail.com>
2025-02-27 19:13:37 +0530

    Completed the project News Application ...Only Search functionality is remaining

--- src/main/java/com/project/News_Application/Controller/NewsController.java
+++ src/main/java/com/project/News_Application/Controller/NewsController.java
@@ -1,12 +1,10 @@
 package com.project.News_Application.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.PathVariable;
 import com.project.News_Application.Entity.News;
 import com.project.News_Application.Services.NewsService;
 
@@ -17,28 +15,40 @@ public class NewsController {
 
      @GetMapping("/")
      public String getNews(Model model) {
-          News news = newsService.getNews();
-          System.out.println(news.toString());
+          News news = newsService.getLatestNews();
+          // System.out.println(news.toString());
           model.addAttribute("News", news);
           return "index";
 
      }
-     //displays trending page
-     @GetMapping("/trending")
-     public String trendingNews(Model model )
-     {
 
+     // displays trending page
+     @GetMapping("/trending")
+     public String trendingNews(Model model) {
+          String category = "top";
+          News news = newsService.getNewsByCategory(category);
+          model.addAttribute("News", news);
           return "Trending";
      }
 
      // display categories page
      @GetMapping("/categories")
-     public String Categories(Model model )
-     {
-
+     public String Categories() {
           return "Category";
      }
-     
-     
+
+     // display categories page
+     @GetMapping("/category/{category}")
+     public String showCategoryNews(@PathVariable String category, Model model) {
+          // Because to get Crypto news it requires to pay the website ..so it is not used
+          // if (category.equals("crypto")) {
+          // News cryptoNews = newsService.getCryptoNews();
+          // model.addAttribute("News", cryptoNews);
+          // return "index";
+          // }
+          News news = newsService.getNewsByCategory(category);
+          model.addAttribute("News", news);
+          return "index";
+     }
 
 }
--- src/main/java/com/project/News_Application/Services/NewsService.java
+++ src/main/java/com/project/News_Application/Services/NewsService.java
@@ -1,14 +1,10 @@
 package com.project.News_Application.Services;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.RuntimeJsonMappingException;
 import com.project.News_Application.Entity.News;
@@ -17,23 +13,48 @@ import com.project.News_Application.Entity.News;
 public class NewsService {
      @Value("${NEWS_API_KEY_2}")
      private String API_KEY;
+     private String baseUrl = "https://newsdata.io/api/1/latest?apikey=";
+
+     // returns latest news
+     public News getLatestNews() {
+          String newUrl = baseUrl + API_KEY + "&language=en";
+          return getNews(newUrl);
+     }
+
+     // returns news by category
+     public News getNewsByCategory(String category) {
+          String newUrl = baseUrl + API_KEY + "&category=" + category;
+          return getNews(newUrl);
+     }
+
+     // returns news by category
+     public News getTrendingNews() {
+          String newUrl = baseUrl + API_KEY + "&";
+          return getNews(newUrl);
+     }
+     // IT NEEDS SUBSCRIPTIONS THATS WHY IT IS NOT USED
+     // // returns crypto news
+     // public News getCryptoNews() {
+     //
+     // String newUrl = "https://newsdata.io/api/1/crypto?apikey=" + API_KEY;
+     // return getNews(newUrl);
+     // }
 
      // Hits the url and get all the news data
-     public News getNews() {
-          // System.out.println(API_KEY);
-          String url = "https://newsdata.io/api/1/latest?apikey=" + API_KEY + "&language=en";
+     private News getNews(String finalUrl) {
+
           // Initialize resttemplate to hit the url
           RestTemplate restTemplate = new RestTemplate();
           // hits url and retrieves data
-          ResponseEntity<String> news = restTemplate.getForEntity(url, String.class);
-          System.out.println("Raw JSON: " + news.getBody());
+          ResponseEntity<String> news = restTemplate.getForEntity(finalUrl, String.class);
+          // System.out.println("Raw JSON: " + news.getBody());
           // If url is successfully hited then alll the data is mapped with java object
           // class
           if (news.getStatusCode() == HttpStatus.OK) {
                try {
                     ObjectMapper objectMapper = new ObjectMapper();
                     News newsData = objectMapper.readValue(news.getBody(), News.class);
-                    System.out.println(newsData.toString());
+                    // System.out.println(newsData.toString());
                     return newsData;
 
                } catch (Exception e) {
